d78e247de2cdeee7df52c3cfccbf72c87af2526a
macymuhia <macymuhia@gmail.com>
2019-12-09 23:04:41 +0300

    Add a memoizer function

--- questionOne/memo.js
+++ questionOne/memo.js
@@ -1,2 +1,15 @@
-// Write a function called memoize that takes any other function as input and returns a memoized version
-//  of that function
+// Write a function called memoize that takes any other function as input
+// and returns a memoized version of that function
+
+function memoize(func) {
+    let cache = {};
+    return function(n) {
+        if (cache[n] != undefined) {
+            return cache[n]
+        } else {
+            let result = func(n);
+            cache[n] = result;
+            return result;
+        }
+    }
+}
