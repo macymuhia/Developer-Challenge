ba4acd6757b1141161c95223b589c90398d53aa7
macymuhia <macymuhia@gmail.com>
2019-12-09 21:03:00 +0300

    Function to test if a number is prime

--- questionOne/prime.js
+++ questionOne/prime.js
@@ -1,1 +1,11 @@
 // Write a function is Prime that returns true if a number is prime.
+
+function isPrime(num) {
+    if (num < 2) return false;
+    for (var i = 2; i <= num / 2; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
